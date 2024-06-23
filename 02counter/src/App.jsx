bd64ccaa6940e1df9203d76542f062cf9ca36cb3
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-06-23 16:54:48 +0530

    add or remove value

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  let [counter, setCount] = useState(0)
+
+  // let counter = 10;
+
+  const addValue = () => {
+    console.log("value added", counter);
+    counter = counter + 1;
+    console.log(setCount(counter));
+  }
+
+  const removeValue = () => {
+    console.log("value remove", counter);
+    counter = counter - 1;
+    console.log(setCount(counter));
+  }
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value </button>
+      <button onClick={removeValue}>Remove value </button>
+    </>
+  );
+}
+
+export default App;
