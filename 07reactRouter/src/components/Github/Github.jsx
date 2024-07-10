3f8eb883a5dba77dcb95fbde0b3d75cbc533a855
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-07-10 13:50:00 +0530

    react router  (mini website)

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //   fetch('https://api.github.com/users/rohitrk29')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //     setData(data);
+    //   })
+    // }, [])
+    
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers:{data.followers}
+
+    <img src={data.avatar_url} alt="imgg" width={200}/>
+    </div>
+ 
+  )
+}
+
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/rohitrk29')
+    return response.json()
+}
+
+
